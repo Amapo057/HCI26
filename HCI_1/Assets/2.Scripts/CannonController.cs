8b62f285bea6304297d0a402eb046141507b317e
Amapo057 <jiseong2002@naver.com>
2026-09-08 10:45:14 +0900

    0908 좌표계

---
+++ HCI_1/Assets/2.Scripts/CannonController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class CannonController : MonoBehaviour
+{
+    public GameObject shellPrefab;
+    public Transform fireTrans;
+    GameObject shell;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            shell = Instantiate(shellPrefab, fireTrans.position, fireTrans.rotation);
+            shell.GetComponent<ShellController>().Shoot(transform.up);
+        }
+    }
+
+}
