45d34ee1e6f246f163794b916f617705f1647ebb
Amapo057 <jiseong2002@naver.com>
2026-09-17 14:47:56 +0900

    Invoke 사용해보기

---
+++ HCI_1/Assets/2.Scripts/Chap 2/TankMoveEvent.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TankMoveEvent : MonoBehaviour
+{
+    public float moveSpeed = 2f;
+    public float rotateSpeed = 40f;
+    float move;
+    float rotate;
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if(Mathf.Abs(move) > 0.1f || Mathf.Abs(rotate) > 0.1f)
+        {
+            Move();
+            Rotate();
+        }
+    }
+
+    // void OnMove(InputValue value)
+    // {
+    //     // Debug.Log("Input value :" + value.Get<float>());    
+    //     move = value.Get<float>();
+    // }
+
+    // void OnRotate(InputValue value)
+    // {
+    //     rotate = value.Get<float>();
+    // }
+
+    public void OnTankMove(InputAction.CallbackContext context)
+    {
+        // Debug.Log("Move value: " + context.ReadValue<float>());    
+        move = context.ReadValue<float>();
+    }
+
+    public void OnTankRotate(InputAction.CallbackContext context)
+    {
+        // Debug.Log("Rotate value: " + context.ReadValue<float>());    
+        rotate = context.ReadValue<float>();
+    }
+
+    void Move()
+    {
+        Vector3 moveDir = transform.forward * move * moveSpeed * Time.deltaTime;
+        rb.MovePosition(rb.position + moveDir);
+        transform.Translate(rb.position + moveDir);
+    }
+
+    void Rotate()
+    {
+        float rotSpeed = rotate * rotateSpeed *Time.deltaTime;
+        rb.rotation = Quaternion.Euler(0, rotSpeed, 0) * rb.rotation;
+    }
+}
